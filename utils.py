e0d6efca1b02d0f4d889ef90d96e033fd74adddf
khk4912 <30457148+khk4912@users.noreply.github.com>
2020-12-12 01:53:11 +0900

    refactor: Drop utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-from model import ApplicationCommandOptionType, SlashCommand
-
-
-def create_command(command: SlashCommand) -> dict:
-    data = {}
-    data["name"] = SlashCommand.name
-
-    return json
