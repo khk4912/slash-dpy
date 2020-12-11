152947ee33c5ed1513962f413a13f272ae4a3d01
khk4912 <30457148+khk4912@users.noreply.github.com>
2020-12-11 19:08:37 +0900

    style: Sort imports

--- slash.py
+++ slash.py
@@ -1,7 +1,8 @@
+from functools import wraps
 from typing import Dict, List, Optional
+
 import discord
 from discord.ext import commands
-from functools import wraps
 
 
 class Slash:
@@ -11,6 +12,5 @@ class Slash:
     def _parse_command_info(self, command: commands.Command) -> None:
         pass
 
-    def _post(self, url, info:Dict[str, str) -> None:
+    def _post(self, url, info: Dict[str, str]) -> None:
         pass
-
